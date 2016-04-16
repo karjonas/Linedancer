e92e1cb8b4be0ae161fe362ed901d3b2b707aad1
Jonas Karlsson <jonaskarlsson@fripost.org>
2016-04-16 14:00:27 +0200

    Add first opponent

--- main.lua
+++ main.lua
@@ -1,15 +1,47 @@
--- Load some default values for our rectangle.
-function love.load(arg)
-    if arg[#arg] == "-debug" then require("mobdebug").start() end
-end
+opponents = {}
  
-user_x = 100
+user_x = 500
 user_speed = 500 -- pixels per second
 
 move_left = false
 move_right = false
+  
+rect_size = 40
+
+function rect_opponent(x)
+  return {speed = 100, x = x, direction = "right"}  
+end
+
+function overlaps(v0,v1,width)
+  v0min = v0 - width/2
+  v0max = v0 + width/2
+  v1min = v1 - width/2
+  v1max = v1 + width/2
+
+  return (v0min <= v1max and v1min <= v0max) 
+end
+
+-- Load some default values for our rectangle.
+function love.load(arg)
+    if arg[#arg] == "-debug" then require("mobdebug").start() end
+    table.insert(opponents, rect_opponent(400))
+end
 
 -- Increase the size of the rectangle every frame.
+function collide()
+  num_pts = table.getn(opponents)
+  
+  for i = 1, num_pts do
+    if overlaps(user_x, opponents[i].x, rect_size) then
+      table.remove(opponents, i)
+      i = i - 1
+      num_pts = num_pts - 1
+    end
+  end  
+
+end
+ 
+
 function love.update(dt)
   
   move_left = love.keyboard.isDown("left") or love.keyboard.isDown("a")
@@ -21,14 +53,24 @@ function love.update(dt)
     user_x = user_x + user_speed*dt
   end
   
+  user_x = math.max(0, user_x)
+  user_x = math.min(love.graphics.getWidth(), user_x)
+  
   move_left = false
   move_right = false
+  
+  for key, value in pairs(opponents) do
+    sign = (value.direction == "left") and -1 or 1
+    value.x = value.x + sign*value.speed*dt
+  end
+  
+  collide()
+  
 end
  
 
 -- x,y is midpoint on rectangle on line
 function calc_user_rect_points(x, y, flip)
-  rect_size = 40
   rect_half = rect_size/2
 
   sign = flip and -1 or 1
@@ -39,7 +81,6 @@ function calc_user_rect_points(x, y, flip)
   p3 = {x = x + rect_half, y = y                  }
   
   return {p0,p1,p2,p3}
-  
 end
 
 function draw_user_rect()
@@ -57,11 +98,24 @@ function draw_user_rect()
   
   for i = 1, num_pts - 1 do
     fst, snd = points[i], points[i+1]
-    love.graphics.line(fst.x, fst.y, snd.x, snd.y)    
-  end
+    love.graphics.line(fst.x, fst.y, snd.x, snd.y)
+  end  
+end
+
+function draw_opponent_rect(opponent)
+  w = love.graphics.getWidth()
+  h = love.graphics.getHeight()
+ 
+  points = calc_user_rect_points(opponent.x, h/2, false)
+ 
+  love.graphics.setColor(100, 100, 0)
   
+  num_pts = table.getn(points)
   
-  
+  for i = 1, num_pts - 1 do
+    fst, snd = points[i], points[i+1]
+    love.graphics.line(fst.x, fst.y, snd.x, snd.y)
+  end  
 end
  
 
@@ -71,5 +125,9 @@ function love.draw()
     h = love.graphics.getHeight()
     
     draw_user_rect()
+    
+    for key, value in pairs(opponents) do
+      draw_opponent_rect(value)
+    end
 
 end
